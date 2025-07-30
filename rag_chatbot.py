4b3a444ab8e71fefd5f54ef1cf0612309147d10c
= <=>
2025-07-30 20:26:57 +0200

    Project setup

---
+++ rag_chatbot.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import os
+import time
+from langchain_groq import ChatGroq
+from langchain_community.llms import Ollama
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from dotenv import load_dotenv
+load_dotenv()
